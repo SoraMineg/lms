da70918a4892bfb7b02481746cb06f74bb1668e7
HAKUSHINNN <103377853+HAKUSHINNN@users.noreply.github.com>
2024-07-03 06:54:52 +0900

    Update LMS Attendance Checker-2024-07-02.user.js

--- LMS Attendance Checker-2024-07-02.user.js
+++ LMS Attendance Checker-2024-07-02.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         LMS Attendance Checker
 // @namespace    http://tampermonkey.net/
-// @version      2024-07-02
+// @version      0.1 beta
 // @description  LMSのマイコース上に出席カウント，後何回休めるのかがわかるダッシュボードを表示します．
 // @author       nihsukah
-// @match        http://*/*
+// @match        https://lms-tokyo.iput.ac.jp/my/courses.php
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -12,9 +12,30 @@
 (function() {
     'use strict';
 
-    // ウインドウの設定
-    const windowStyle =
-          position = fixed;
+    // ウインドウのスタイルを設定する
+    const windowStyle = `
+        position: fixed;
+        top: 50px;
+        right: 50px;
+        width: 300px;
+        height: 200px;
+        background: white;
+        border: 1px solid black;
+        padding: 10px;
+        z-index: 1000;
+    `;
 
-    ;
+    // ウインドウのHTMLを生成する
+    const attendanceWindow = document.createElement('div');
+    attendanceWindow.setAttribute('style', windowStyle);
+    attendanceWindow.innerHTML = `
+        <h3>出席回数記録</h3>
+        <label for="attendanceCount">出席回数:</label>
+        <input type="number" id="attendanceCount" min="0" value="0" style="width: 50px;">
+        <button id="saveAttendance">保存</button>
+        <button id="closeWindow">閉じる</button>
+    `;
+
+    // ウインドウをページに追加する
+    document.body.appendChild(attendanceWindow);
 })();
