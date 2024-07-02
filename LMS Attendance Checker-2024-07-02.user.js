65632ed9f7b4e5c3ed9fcb61c26dc43a3708edf4
HAKUSHINNN <103377853+HAKUSHINNN@users.noreply.github.com>
2024-07-02 20:05:38 +0900

    Add files via upload

---
+++ LMS Attendance Checker-2024-07-02.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         LMS Attendance Checker
+// @namespace    http://tampermonkey.net/
+// @version      2024-07-02
+// @description  LMSのマイコース上に出席カウント，後何回休めるのかがわかるダッシュボードを表示します．
+// @author       nihsukah
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // ウインドウの設定
+    const windowStyle =
+          position = fixed;
+
+    ;
+})();
